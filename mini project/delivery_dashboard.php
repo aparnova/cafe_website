60da5469dc378b6f5b35386be043f290c3eff068
Aparna <aparnapmadhu7@gmail.com>
2025-08-20 23:43:15 +0530

    Update delivery_dashboard.php

--- mini project/delivery_dashboard.php
+++ mini project/delivery_dashboard.php
@@ -3,7 +3,7 @@ session_start();
 require 'db.php';
 
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'delivery') {
-    header("Location: login.php");
+    header("Location: homepage.php");
     exit();
 }
 ?>
